2814903c034e057d5bd23154665b87b2079fabad
puxir <80805649+puxir@users.noreply.github.com>
2021-05-13 09:30:52 -0300

    Update fixpix.sh

--- src/fixpix.sh
+++ src/fixpix.sh
@@ -9,6 +9,8 @@ for old_fname in `ls`
 do
 
   mogrify -format jpg -quality 72 -resize 800x450\! -colorspace Gray $old_fname
-  rm -f $old_fname
+  
+  new_fname=$newres.`md5sum $old_fname | cut -d' ' -f1`.jpeg
+  mv -f $old_fname $new_fname
 
 done
